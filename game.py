0d73c1bf02a3bc443ffde11607acb86d01c193e8
Martin Evtimov <martinevtimov03@abv.bg>
2024-11-28 21:18:24 +0200

    -- added logic for main menu

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame, sys, json
 from pygame.locals import*
-from player import Player
+from menu.menu import Menu
 from world import World
 from tile import *
 
@@ -11,29 +11,62 @@ class Game():
         
         pygame.display.set_caption('Game')
         DISPLAY_W, DISPLAY_H = 1920, 1080
+        
 
         with open("level1-map.json") as f:
             self.level_data = json.load(f)
         f.close()
+        
+        self.game_paused = False
+        self.menu_state = "main"
         
         self.screen = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
         self.display = pygame.Surface((DISPLAY_W, DISPLAY_H), pygame.SRCALPHA)
         self.FPS = 60
         self.fpsClock = pygame.time.Clock()
 
+        # menu
+        self.menu = Menu(DISPLAY_W, DISPLAY_H)
+        
         # level init
-        self.level = World(self.screen, self.display)
+        self.level = World(self.screen, self.display, self.level_data)
         
             
     def run(self):
         while True:
             
+            if self.game_paused == True:
+                if self.menu_state == "main":
+                    self.screen.fill((0, 0, 0))
+                    if self.menu.resume_button.draw(self.screen):
+                        self.game_paused = False
+                    if self.menu.options_button.draw(self.screen):
+                        self.menu_state = "options"
+                    if self.menu.quit_button.draw(self.screen):
+                        pygame.quit()
+                        sys.exit()
+                if self.menu_state == "options":
+                    self.screen.fill((0, 0, 0))
+                    if self.menu.video_button.draw(self.screen):
+                        print("nothing")
+                    if self.menu.audio_button.draw(self.screen):
+                        print("nothing")
+                    if self.menu.keys_button.draw(self.screen):
+                        print("nothing")
+                    if self.menu.back_button.draw(self.screen):
+                        self.menu_state = "main"
+            else:
+                self.level.run()
+            
             for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.game_paused = True
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
                 
-            self.level.run()
+            
                 
                 
             pygame.display.update()
