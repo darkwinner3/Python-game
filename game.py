fe2ec89847da1b82fa6979a3616f56b6805641f8
Martin Evtimov <martinevtimov03@abv.bg>
2024-11-29 22:23:57 +0200

    -- added scroll function -- added function for window resizing

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
-import pygame, sys, json
+import pygame, sys, json, os
 from pygame.locals import*
 from menu.menu import Menu
 from world import World
 from tile import *
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
 
 class Game():
     def __init__(self):
@@ -17,51 +19,47 @@ class Game():
             self.level_data = json.load(f)
         f.close()
         
-        self.game_paused = False
-        self.menu_state = "main"
-        
         self.screen = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
         self.display = pygame.Surface((DISPLAY_W, DISPLAY_H), pygame.SRCALPHA)
         self.FPS = 60
         self.fpsClock = pygame.time.Clock()
 
         # menu
-        self.menu = Menu(DISPLAY_W, DISPLAY_H)
+        self.menu = Menu(DISPLAY_W, DISPLAY_H, self.screen, self)
         
         # level init
         self.level = World(self.screen, self.display, self.level_data)
-        
+    
+    def change_screen_size(self, width, height):
+        # Update screen and display
+        self.DISPLAY_W, self.DISPLAY_H = width, height
+        self.screen = pygame.display.set_mode((width, height))
+        self.display = pygame.Surface((width, height), pygame.SRCALPHA)
+
+        # Notify other components
+        self.menu.update_resolution(width, height, self.screen)
+        self.level.update_resolution(width, height, self.screen)   
             
     def run(self):
         while True:
             
-            if self.game_paused == True:
-                if self.menu_state == "main":
-                    self.screen.fill((0, 0, 0))
-                    if self.menu.resume_button.draw(self.screen):
-                        self.game_paused = False
-                    if self.menu.options_button.draw(self.screen):
-                        self.menu_state = "options"
-                    if self.menu.quit_button.draw(self.screen):
-                        pygame.quit()
-                        sys.exit()
-                if self.menu_state == "options":
-                    self.screen.fill((0, 0, 0))
-                    if self.menu.video_button.draw(self.screen):
-                        print("nothing")
-                    if self.menu.audio_button.draw(self.screen):
-                        print("nothing")
-                    if self.menu.keys_button.draw(self.screen):
-                        print("nothing")
-                    if self.menu.back_button.draw(self.screen):
-                        self.menu_state = "main"
+            if self.menu.game_paused:
+                self.menu.run(self.screen)
             else:
                 self.level.run()
             
             for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.game_paused = True
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    self.menu.menu_state = "main"
+                    self.menu.game_paused = not self.menu.game_paused
+                if event.type == pygame.MOUSEWHEEL:
+                    if event.y > 0 and self.menu.scroll_offset > 0:
+                        self.menu.scroll_offset -= self.menu.scroll_step
+                    elif event.y < 0 and self.menu.scroll_offset < len(self.menu.menus["levels"]) - self.menu.max_visible_buttons:
+                        self.menu.scroll_offset += self.menu.scroll_step
+                if event.type == pygame.VIDEORESIZE:
+                    new_width, new_height = event.w, event.h
+                    self.change_screen_size(new_width, new_height)
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
